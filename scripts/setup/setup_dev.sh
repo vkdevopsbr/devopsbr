0717a35d23ecbad7ef232f1fd9616f0c51fd50a3
Victor Hugo Lima <147187329+vkdevopsbr@users.noreply.github.com>
2024-08-05 23:58:52 -0300

    Update setup_dev.sh

--- scripts/setup/setup_dev.sh
+++ scripts/setup/setup_dev.sh
@@ -25,8 +25,8 @@ install_go() {
   echo "Instalando Go..."
   wget https://dl.google.com/go/go1.16.7.linux-amd64.tar.gz
   tar -C /usr/local -xzf go1.16.7.linux-amd64.tar.gz
-  echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.profile
-  source ~/.profile
+  echo "export PATH=\$PATH:/usr/local/go/bin" >> /etc/profile
+  source /etc/profile
   rm go1.16.7.linux-amd64.tar.gz
 }
 
